842a42d5f4572b20a008a93068e896b84a64b70d
IDRISSI Mohamed <mohamed.idrissi@4d.com>
2014-02-15 23:46:12 +0000

    Update simple.js

--- test/simple.js
+++ test/simple.js
@@ -1,1 +1,23 @@
-﻿var Redis = require('redis');var r = new Redis({	tries: {		count:-1	}});r.on({	'socket:data': function(){		//debugger;	},	'socket:error': function(){		//debugger;	},	'socket:connect': function(){		//debugger;	},	'socket:close': function(){		//debugger;	},	'socket:fail': function(){		exitWait();	}});var res;r.auth('foobared',function(data, error){	debugger;});r.ping(function(data, error){	debugger;});r.get('foo',function(data, error){	debugger;	exitWait();});wait();res;
+var Redis = require('redis-wakanda');
+
+var r = new Redis();
+
+var res;
+
+// Authenticate to the server
+r.auth('foobared');
+
+// Ping the server
+r.ping(function(data, error){
+// The data must contain "PONG"
+});
+
+// Get the foo key
+r.get('foo',function(data, error){
+res = data;
+exitWait();
+});
+wait();
+
+// res must contain the value of the key "foo"
+res;
